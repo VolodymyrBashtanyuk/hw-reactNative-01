34a9964baa0a3517c4d0cecc84d01e60f66dca9a
Volodymyr Bashtanyuk <volodymyrbashtanyuk@MacBook-Air-Volodymyr.local>
2023-05-12 15:47:27 +0300

    add background image

--- myMobileApp/App.js
+++ myMobileApp/App.js
@@ -1,39 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-// import { TextInput } from 'react-native-web'
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.textBox}>
-        <Text style={styles.text}>My first App!!!Started!!!!Learning React-Native?</Text>
-        <Text style={styles.text}>Program started :)!!!</Text>
-        <Text style={styles.text}>Congratulation!!!!!!</Text>
-      </View>
-     
-      <StatusBar style="auto" />
+      <ImageBackground style={styles.image} source={require('./assets/images/image-app.jpeg')}>
+          <View style={styles.textBox}>
+            <Text style={styles.text}>My first App!!!Started!!!!Learning React-Native?</Text>
+            <Text style={styles.text}>Program started :)!!!</Text>
+            <Text style={styles.text}>Congratulation!!!!!!</Text>
+          </View>
+
+        <StatusBar style="auto" />
+      </ImageBackground>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    paddingLeft: 2,
-    paddingRight: 2,
+    
   },
   text: {
-    color: '#3c3c1f',
+    color: '#fee1d3',
     fontSize: 30,
   },
   textBox: {
     borderWidth: 2,
-    borderColor: '#2200fe',
-    padding: 30,
+    borderColor: '#fd3412',
+    padding: 20,
     borderRadius: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    resizeMode: 'cover',
   }
 });
