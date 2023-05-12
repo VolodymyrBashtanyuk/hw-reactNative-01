10e1b2e29aed5dc8e3174296ac4a896d2bd576fd
Volodymyr Bashtanyuk <volodymyrbashtanyuk@MacBook-Air-Volodymyr.local>
2023-05-12 14:56:06 +0300

    add text and styles

--- myMobileApp/App.js
+++ myMobileApp/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import { TextInput } from 'react-native-web'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My first App!!!Started!!!!мама хочиш фокус?</Text>
-      <Text>Дивись як я можу Ура!!!</Text>
+      <View style={styles.textBox}>
+        <Text style={styles.text}>My first App!!!Started!!!!Learning React-Native?</Text>
+        <Text style={styles.text}>Program started :)!!!</Text>
+        <Text style={styles.text}>Congratulation!!!!!!</Text>
+      </View>
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +22,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',
+    paddingLeft: 2,
+    paddingRight: 2,
   },
+  text: {
+    color: '#3c3c1f',
+    fontSize: 30,
+  },
+  textBox: {
+    borderWidth: 2,
+    borderColor: '#2200fe',
+    padding: 30,
+    borderRadius: 20,
+  }
 });
